a38a49d10ae96d707403b7129c103da1582baa19
edy <0roroe13@solent.ac>
2024-10-26 13:31:10 +0100

    Adding new folders - New

---
+++ Week1/input.py
@@ -0,0 +1,19 @@
+print("please enter your name")
+name = input()
+print(f"your name is {name}")
+
+print("please enter your age")
+age = int(input())
+print(f"your age is {age}")
+
+print("plese enter your weight")
+weight = float(input())
+print(f"your weight is {weight}")
+
+print("please enter your height")
+height = float(input())
+print(f"your height is {height}")
+
+bmi = weight / (height * height)
+
+print(f"your bmi is {bmi}")
---
+++ Week1/test1.py
@@ -0,0 +1,13 @@
+a = 10
+b = 5
+c = "five"
+
+sum_result = a + b
+difference_result = a-b
+product_result = a*b
+division_result = a/b
+
+print("sum", a+b)
+print("difference", a-b)
+print("product", a*b)
+print("division", a/b)
